415c7abe246ca314df6966d4c81f3f13fcfc3966
babladey275 <babladey275@gmail.com>
2025-01-16 12:35:13 +0530

    added JWT token handling and Axios interceptors

--- src/hooks/UseAxiosSecure.jsx
+++ src/hooks/UseAxiosSecure.jsx
@@ -1,10 +1,45 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const UseAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
+  // request interceptor to add authorization header for every secure call to the api
+  axiosInstance.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  //intercepts 401 and 403 status
+  axiosInstance.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("status error in the interceptor", status);
+
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiosInstance;
 };
 
